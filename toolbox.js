478bf8d39c82d375962a2936ce72eb23cf4ea670
thilina <thilinaabeysinghe@gmail.com>
2016-05-25 10:05:29 -0400

    File Open Dialog Set working mode

--- toolbox.js
+++ toolbox.js
@@ -1,3 +1,13 @@
 function openfileDialog() {
-   	document.getElementById("fileLoader").click();
+   	document.getElementById("fileLoader").click(); 	
+
+
+	var control = document.getElementById("fileLoader");
+   	control.addEventListener("change",function(event){
+   		console.log("Hello");
+   		files = control.files,
+   		len = files.length;
+
+   		console.log(files[0].name);
+   	});	
 }
